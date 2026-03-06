8e3b52a90916bca589e030adf18dfca9083b1505
aayushsuwalka <aayush.suwalka08@gmail.com>
2026-03-06 21:06:07 +0530

    Add files via upload

---
+++ Day_7/Day7.py
@@ -0,0 +1,11 @@
+class Car:
+    wheels = 4
+    engine = 'Petrol'
+    base_speed = '40kmph'
+    max_speed = '120kmph'
+    gears = 4
+
+TATA = Car()
+Mahindra = Car()
+Suzuki = Car()
+Toyota = Car()
